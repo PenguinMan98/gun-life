1e682b31c175630651ae0a86f2546938fc6fdc4d
Joseph Broderick <penguinman98@usa.net>
2017-11-02 02:14:50 -0600

    Added the methods for toggleState, increase-decreaseLiveNeighborCount, and tick. Added the click listener. Tied them all to their html elements. Click now toggles their state.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { app, BrowserWindow } = electron;
 
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    height: 1000,
+    width: 1600
+  });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
